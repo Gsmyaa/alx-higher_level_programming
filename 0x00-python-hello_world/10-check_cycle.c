7c74b41f7baf7facb20a30b098bb6a8f74e44879
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-03-08 04:48:43 +0300

    all task done

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,25 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a cycle exists
- * @list: linked list to be check
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
  *
- * Return: 1 if a cycle exists, 0 if it doesn't
+ * Return: 1 if the list has a cycle, 0 if it doesn't
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *sound = list;
-	listint_t *light = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
 	if (!list)
 		return (0);
 
-	while (sound && light && light->next)
+	while (slow && fast && fast->next)
 	{
-		sound = sound->next;
-		light = light->next->next;
-		if (sound == light)
-		{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 			return (1);
-		}
 	}
 
 	return (0);
