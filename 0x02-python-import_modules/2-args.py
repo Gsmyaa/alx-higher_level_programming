cf3402b90e1046853e5bdeda2bed08470a4384eb
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-03-11 15:41:47 +0300

    task 2 done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     n = len(sys.argv)
     if n - 1 == 0:
         print("{} arguments.".format(n - 1))
-    if n - 1 > 0:
+    else:
         print("{} arguments:".format(n - 1))
     for i in range(1, n):
         print("{}: {}".format(i, str(sys.argv[i])))
