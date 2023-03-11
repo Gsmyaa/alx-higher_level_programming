f7651ee696479df1f2916363a2e25bd5140fecf0
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-03-11 13:26:25 +0300

    task 1 done

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+#compute addition
+    print("{} + {} = {}".format(a, b, add(a, b)))
+#compute substraction
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+#compute multiplication
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+#compute division
+    print("{} / {} = {}".format(a, b, div(a, b)))
