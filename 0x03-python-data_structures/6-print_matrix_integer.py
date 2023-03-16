bd22103ca868842f971b21a8f58677faa117eac5
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-03-16 22:44:23 +0300

    task 6 done

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
+    if matrix != [[]]:
         for i in matrix:
             for j in range(len(i)):
                 if j < len(i) - 1:
