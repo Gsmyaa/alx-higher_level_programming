08fd7f9a75ac4ae4e5c7f2a4300b989ac6df0784
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-03-17 17:46:40 +0300

    task 9 done

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list:	
+    if my_list:
         larg = my_list[0]
         for i in range(len(my_list)):
             if my_list[i] > larg:
@@ -8,4 +8,3 @@ def max_integer(my_list=[]):
         return larg
     else:
         return None
-    
