f934060bb4774d4941b8706e1d6b78713d7ecd0e
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-03-17 17:44:18 +0300

    task 9 done

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:	
+        larg = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > larg:
+                larg = my_list[i]
+        return larg
+    else:
+        return None
+    
