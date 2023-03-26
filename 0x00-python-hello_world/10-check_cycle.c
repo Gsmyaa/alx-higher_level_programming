a0e51bbdd5ad794210a66245d4a4377d85b31323
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-03-26 20:27:36 +0300

    task 10 done

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,3 +1,25 @@
 #include "lists.h"
 
+/**
+ * check_cycle - checks if there is a cycle
+ * @list: linked list
+ * Return: 1 if there is a cycle, 0 if no cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
 
+	if (!list)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
