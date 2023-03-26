2183c59d46892c1a1935cb8cce51e2f8ac11679a
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-03-26 20:36:41 +0300

    task 2, 8 and 9

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
 
-    n = len(sys.argv)
-    if n - 1 == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{:d} arguments:".format(n - 1))
-    for i in range(1, n):
-        print("{:d}: {}".format(i, sys.argv[i]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
