133e6ff7a64f8135fd0f57b7f4a12fe33d137e28
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-03-30 04:27:31 +0300

    task 2 done

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Define a class Square
+"""
+
+
+class Square:
+    """
+    initialize object with private
+    instances.
+    """
+    def __init__(self, size=0):
+        """
+        Args:
+            size: size of the new square.
+        """
+        self.__size = size
+        try:
+            assert type(size) == int
+        except:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
