8f44e55ec76caf21f488726a9136635235e4e4d9
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-03-30 03:49:55 +0300

    task 1 done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Instantiation with size"""
+    def __init__(self, size):
+        """Args:
+                size(int): size of new square.
+        """
+        self.__size == size
