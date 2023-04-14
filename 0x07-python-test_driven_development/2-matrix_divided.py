cfaaa868df27ee361dd251b24b9598060f197572
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-04-14 03:33:15 +0300

    task 0, 2, 3 is done

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,7 +5,7 @@
 def matrix_divided(matrix, div):
     """Returns a new matrix, product of matrix division by int
 
-    Matrix must be a list of lists of integers or floats, 
+    Matrix must be a list of lists of integers or floats,
     otherwise raise a TypeError.
 
     div:
@@ -17,16 +17,18 @@ def matrix_divided(matrix, div):
     for i in matrix:
         for j in i:
             if (not isinstance(j, int) and not isinstance(j, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a 
+                     matrix (list of lists) of integers/floats")
     row_len = []
     for i in matrix:
         row_len.append(len(i))
     for i in matrix:
         for j in row_len:
             if len(i) != j:
-                raise TypeError("Each row of the matrix must have the same size")
+                raise TypeError("Each row of the 
+                     matrix must have the same size")
     if (not isinstance(div, int) and not isinstance(div, float)):
-         raise TypeError("div must be a number")
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     new_matrix = []
