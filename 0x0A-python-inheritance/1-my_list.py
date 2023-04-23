659ce01e0de713671234021909fd193328977c71
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-04-24 00:18:08 +0300

    task 1 done

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines module that inherit from list"""
 
+
 class MyList(list):
-    """Implement sorted for printing built-in list class"""
+    """Implement sorted forprinting built-in list class"""
     def print_sorted(self):
         """prints the sorted list"""
         print(sorted(self))
-    
