d80873daa1340caf4af0a302934d8764d7288963
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-05-11 17:19:02 +0300

    task 5 update done

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,6 +12,5 @@ def save_to_json_file(my_obj, filename):
         filename - name of file
     returns: nothing
     """
-    my_str = json.dumps(my_obj)
     with open(filename, encoding='utf-8', mode='w') as f:
-        f.write(my_str)
+        json.dump(my_obj, f)
