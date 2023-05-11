fcd0ebe52fe952771b57d10922f6b53d6b32df45
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-05-11 16:39:55 +0300

    task 10 done

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Defines class a student"""
+"""module that defines a class student"""
 
 
 class Student:
-    """defines a student"""
+    """represent a student"""
     def __init__(self, first_name, last_name, age):
-        """instantiation"""
+        """initialize a student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """retrieves a dictionary representation of
-        Student instance"""
+        Student instance
+        if attrs is list of strings, include only attributes
+        """
         if type(attrs) == list and all(type(elem) == str for elem in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
