a6a676bfaee1e57890fdd0b7bb2c530b4e4fb7c1
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-05-11 15:16:33 +0300

    task 9 done

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines module that define student class"""
+
+
+class Student:
+    """Defines a class student"""
+    def __init__(self, first_name, last_name, age):
+        """inistatiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        """retrieves a dictionary"""
+        return self.__dict__
