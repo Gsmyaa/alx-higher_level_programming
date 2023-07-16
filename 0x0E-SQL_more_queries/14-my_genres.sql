adca778134083ce78fcbe2cc56f19e3c716ad25b
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-07-17 01:25:04 +0300

    update task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,7 +4,7 @@
 LEFT JOIN tv_show_genres 
        ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows 
-       ON tv_show_genres.tv_show_id = tv_shows.id
+       ON tv_show_genres.show_id = tv_shows.id
     WHERE tv_shows.title = 'Dexter'
     GROUP BY name
     ORDER BY name ASC;
