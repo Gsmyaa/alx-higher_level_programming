efd88febf3f8f8ba1619666f66d29ad61be5f20c
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-08-20 13:04:07 +0300

    tasks done

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,10 +10,14 @@ import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-        db=sys.argv[3], port=3306)
+                         db=sys.argv[3], port=3306)
 
     curs = db.cursor()
-    curs.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+    curs.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
     states = curs.fetchall()
 
     for state in states:
