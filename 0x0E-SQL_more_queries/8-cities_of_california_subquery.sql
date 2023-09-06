34da00ba38d0c5cbfe325daabcb3460260abc45b
Gsmyaa <106983078+Gsmyaa@users.noreply.github.com>
2023-09-06 11:35:47 +0300

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++
@@ -1,9 +0,0 @@
--- a script that lists all the cities of California 
-SELECT id, name
-  FROM cities
- WHERE state_id = (
-       SELECT id 
-	 FROM states 
-	WHERE name = "California"
-   )
- ORDER BY id;
--- 0x0E-SQL_more_queries/README.md
+++
@@ -1,1 +0,0 @@
-these are tasks to be done for project '0x0E-SQL_more_queries'
